6a5e910e47339f3ace554ac812f53c92db736ab0
devkaare <kaarehofferdal@gmail.com>
2025-03-08 03:13:46 +0100

    Feat

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -44,6 +44,7 @@ return {
       json = { 'prettierd', 'prettier', stop_after_first = true },
       markdown = { 'prettierd', 'prettier', stop_after_first = true },
       html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }

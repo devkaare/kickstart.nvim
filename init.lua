d1188fbdd70d901a2bde0e113d9db07678e7a92e
devkaare <kaarehofferdal@gmail.com>
2025-03-23 16:23:20 +0100

    Feat

--- init.lua
+++ init.lua
@@ -40,6 +40,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('i', '<C-e>', 'if err != nil {\n}')
+
 -- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 -- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
